db7f2bf28568a55e19687daf805f568676eddc76
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-07-28 00:33:45 +0200

    connectivity checks

--- app/src/main/java/com/trianglz/task/usersmain/presentation/MainActivity.kt
+++ app/src/main/java/com/trianglz/task/usersmain/presentation/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
     }
 
     private fun initViews(){
